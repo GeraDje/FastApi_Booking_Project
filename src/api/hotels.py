d8ef8a5c5afd1ab1f7bb5fd5954ca8defb96c6cc
 mac <mac@MacBookPro.Dlink>
2026-02-05 18:27:41 -0800

    добавил edit and delet методы в BaseRepository, использую из в PUT /hotels и DELETE /hotels

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,6 +1,7 @@
 from fastapi import Query, APIRouter, Body
 
 from sqlalchemy import insert, select, func
+from watchfiles import awatch
 
 from src.api.dependencies import PaginationDep
 from src.database import async_session_maker
@@ -27,7 +28,6 @@ async def get_hotels(
         )
 
 
-
 @router.post("")
 async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     "1": {
@@ -49,15 +49,14 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     async with async_session_maker() as session:
         hotel = await HotelsRepository(session).add(hotel_data)
         await session.commit()
-    return {"status": "OK", "data":hotel}
+    return {"status": "OK", "data": hotel}
 
 
 @router.put("/{hotel_id}")
-def edit_hotel(hotel_id: int, hotel_data: Hotel):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    hotel["title"] = hotel_data.title
-    hotel["name"] = hotel_data.name
+async def edit_hotel(hotel_id: int, hotel_data: Hotel):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(hotel_data, id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
 
 
@@ -80,7 +79,8 @@ def partially_edit_hotel(
 
 
 @router.delete("/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+async def delete_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).delete(id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
