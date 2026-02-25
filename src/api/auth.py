9d504380f8fde541ae15532611b976d439e66eac
 mac <mac@MacBook-Pro-mac.local>
2026-02-25 13:26:06 +0300

    поменял async_session_maker на DBDep

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Response, Request
 
-from src.api.dependencies import UserIdDep
+from src.api.dependencies import UserIdDep, DBDep
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
@@ -24,18 +24,18 @@ async def register_user(
 
 @router.post("/login")
 async def login_user(
+        db: DBDep,
         data: UserRequestAdd,
         response: Response,
 ):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
-        if not user:
-            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
-        if not AuthService().verify_password(data.password, user.hashed_password):
-            raise HTTPException(status_code=401, detail="Пароль неверный")
-        access_token = AuthService().create_access_token({"user_id": user.id})
-        response.set_cookie("access_token", access_token)
-        return {"access_token": access_token}
+    user = await db.users.get_user_with_hashed_password(email=data.email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
+    if not AuthService().verify_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Пароль неверный")
+    access_token = AuthService().create_access_token({"user_id": user.id})
+    response.set_cookie("access_token", access_token)
+    return {"access_token": access_token}
 
 
 @router.get("/logout")
@@ -43,9 +43,7 @@ async def logout_user(response: Response):
     response.delete_cookie("access_token")
 
 
-
 @router.post("/me")
-async def get_me(request: Request, user_id: UserIdDep):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(id=user_id)
-        return user
+async def get_me(db:DBDep, user_id: UserIdDep):
+    user = await db.users.get_one_or_none(id=user_id)
+    return user
--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,7 +1,7 @@
 from fastapi import Query, APIRouter, Body
 
 from src.repositories.hotels import HotelsRepository
-from src.api.dependencies import PaginationDep
+from src.api.dependencies import PaginationDep, DBDep
 from src.database import async_session_maker
 from src.schemas.hotels import HotelPatch, HotelAdd
 
@@ -11,28 +11,29 @@ router = APIRouter(prefix="/hotels", tags=["Отели"])
 @router.get("")
 async def get_hotels(
         pagination: PaginationDep,
-        db: DBDep
+        db: DBDep,
         location: str | None = Query(None, description="Локация"),
         title: str | None = Query(None, description="Название отеля"),
 ):
     per_page = pagination.per_page or 5
-
-    return await HotelsRepository(session).get_all(
-            location=location,
-            title=title,
-            limit=per_page,
-            offset=per_page * (pagination.page - 1)
-        )
+    return await db.hotels.get_all(
+        location=location,
+        title=title,
+        limit=per_page,
+        offset=per_page * (pagination.page - 1)
+    )
 
 
 @router.get("/{hotel_id}")
-async def get_hotel(hotel_id: int):
-    async with async_session_maker() as session:
-        return await HotelsRepository(session).get_one_or_none(id=hotel_id)
+async def get_hotel(
+        hotel_id: int,
+        db: DBDep,
+):
+    return await db.hotels.get_one_or_none(id=hotel_id)
 
 
 @router.post("")
-async def create_hotel(hotel_data: HotelAdd = Body(openapi_examples={
+async def create_hotel(db: DBDep, hotel_data: HotelAdd = Body(openapi_examples={
     "1": {
         "summary": "Сочи",
         "value": {
@@ -47,20 +48,22 @@ async def create_hotel(hotel_data: HotelAdd = Body(openapi_examples={
             "location": "ул. Шейха, 2",
         }
     }
-})
-):
-    async with async_session_maker() as session:
-        hotel = await HotelsRepository(session).add(hotel_data)
-        await session.commit()
+}),
+                       ):
+    hotel = await db.hotels.add(hotel_data)
+    await db.commit()
 
     return {"status": "OK", "data": hotel}
 
 
 @router.put("/{hotel_id}")
-async def edit_hotel(hotel_id: int, hotel_data: HotelAdd):
-    async with async_session_maker() as session:
-        await HotelsRepository(session).edit(hotel_data, id=hotel_id)
-        await session.commit()
+async def edit_hotel(
+        db: DBDep,
+        hotel_id: int,
+        hotel_data: HotelAdd
+):
+    await db.hotels.edit(hotel_data, id=hotel_id)
+    await db.commit()
     return {"status": "OK"}
 
 
@@ -72,16 +75,15 @@ async def edit_hotel(hotel_id: int, hotel_data: HotelAdd):
 async def partially_edit_hotel(
         hotel_id: int,
         hotel_data: HotelPatch,
+        db: DBDep,
 ):
-    async with async_session_maker() as session:
-        await HotelsRepository(session).edit(hotel_data, exclude_unset=True, id=hotel_id)
-        await session.commit()
+    await db.hotels.edit(hotel_data, exclude_unset=True, id=hotel_id)
+    await db.commit()
     return {"status": "OK"}
 
 
 @router.delete("/{hotel_id}")
-async def delete_hotel(hotel_id: int):
-    async with async_session_maker() as session:
-        await HotelsRepository(session).delete(id=hotel_id)
-        await session.commit()
+async def delete_hotel(hotel_id: int, db: DBDep):
+    await db.hotels.delete(id=hotel_id)
+    await db.commit()
     return {"status": "OK"}
