a7c3717d239f1dc11dccb1aee97460a0f5a57995
 mac <mac@MacBook-Pro-mac.local>
2026-02-25 15:53:29 +0300

    add GET /bookings, GET /bookings/me

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -5,26 +5,29 @@ from src.schemas.bookings import BookingAddRequest, BookingAdd
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 
-@router.post("/")
+@router.get("")
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+
+@router.get("/me")
+async def get_my_bookings(user_id: UserIdDep, db: DBDep):
+    return await db.bookings.get_filtered(user_id=user_id)
+
+
+@router.post("")
 async def add_booking(
         user_id: UserIdDep,
         db: DBDep,
         booking_data: BookingAddRequest,
-
 ):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
-    print(f"--------{room}---------")
     room_price: int = room.price
-    print(f"--------{room_price}---------")
     _booking_data = BookingAdd(
         user_id=user_id,
         price=room_price,
-        **booking_data.model_dump(),
+        **booking_data.dict(),
     )
-    print(f"--------{_booking_data}---------")
     booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
-
-@router.post("/")
-async def get_bookings():
