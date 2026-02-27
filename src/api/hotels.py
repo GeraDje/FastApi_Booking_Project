665fabdaa34abea3d8c88f5a6f1f1bf2b165b973
 mac <mac@MacBook-Pro-mac.local>
2026-02-27 11:07:01 +0300

    create get_filtered_by_time() for HotelsRepository

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,8 +1,8 @@
+from datetime import date
+
 from fastapi import Query, APIRouter, Body
 
-from src.repositories.hotels import HotelsRepository
 from src.api.dependencies import PaginationDep, DBDep
-from src.database import async_session_maker
 from src.schemas.hotels import HotelPatch, HotelAdd
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
@@ -14,42 +14,47 @@ async def get_hotels(
         db: DBDep,
         location: str | None = Query(None, description="Локация"),
         title: str | None = Query(None, description="Название отеля"),
+        date_from: date = Query(example="2024-08-01"),
+        date_to: date = Query(example="2024-08-10"),
 ):
     per_page = pagination.per_page or 5
-    return await db.hotels.get_all(
-        location=location,
-        title=title,
-        limit=per_page,
-        offset=per_page * (pagination.page - 1)
+    # return await db.hotels.get_all(
+    #     location=location,
+    #     title=title,
+    #     limit=per_page,
+    #     offset=per_page * (pagination.page - 1)
+    # )
+    return await db.hotels.get_filtered_by_time(
+        date_from=date_from,
+        date_to=date_to,
     )
 
 
 @router.get("/{hotel_id}")
-async def get_hotel(
-        hotel_id: int,
-        db: DBDep,
-):
+async def get_hotel(hotel_id: int, db: DBDep):
     return await db.hotels.get_one_or_none(id=hotel_id)
 
 
 @router.post("")
-async def create_hotel(db: DBDep, hotel_data: HotelAdd = Body(openapi_examples={
-    "1": {
-        "summary": "Сочи",
-        "value": {
-            "title": "Отель Сочи 5 звезд у моря",
-            "location": "ул. Моря, 1",
-        }
-    },
-    "2": {
-        "summary": "Дубай",
-        "value": {
-            "title": "Отель Дубай У фонтана",
-            "location": "ул. Шейха, 2",
-        }
-    }
-}),
-                       ):
+async def create_hotel(
+        db: DBDep,
+        hotel_data: HotelAdd = Body(openapi_examples={
+            "1": {
+                "summary": "Сочи",
+                "value": {
+                    "title": "Отель Сочи 5 звезд у моря",
+                    "location": "ул. Моря, 1",
+                }
+            },
+            "2": {
+                "summary": "Дубай",
+                "value": {
+                    "title": "Отель Дубай У фонтана",
+                    "location": "ул. Шейха, 2",
+                }
+            }
+        })
+):
     hotel = await db.hotels.add(hotel_data)
     await db.commit()
 
@@ -57,11 +62,7 @@ async def create_hotel(db: DBDep, hotel_data: HotelAdd = Body(openapi_examples={
 
 
 @router.put("/{hotel_id}")
-async def edit_hotel(
-        db: DBDep,
-        hotel_id: int,
-        hotel_data: HotelAdd
-):
+async def edit_hotel(hotel_id: int, hotel_data: HotelAdd, db: DBDep):
     await db.hotels.edit(hotel_data, id=hotel_id)
     await db.commit()
     return {"status": "OK"}
--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,7 +1,11 @@
-from src.models.hotels import HotelsOrm
-from src.repositories.base import BaseRepository
+from datetime import date
+
 from sqlalchemy import select, func
 
+from src.models.rooms import RoomsOrm
+from src.repositories.base import BaseRepository
+from src.models.hotels import HotelsOrm
+from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.hotels import Hotel
 
 
@@ -15,7 +19,7 @@ class HotelsRepository(BaseRepository):
             title,
             limit,
             offset,
-    ):
+    ) -> list[Hotel]:
         query = select(HotelsOrm)
         if location:
             query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
@@ -26,6 +30,20 @@ class HotelsRepository(BaseRepository):
             .limit(limit)
             .offset(offset)
         )
+        print(query.compile(compile_kwargs={"literal_binds": True}))
         result = await self.session.execute(query)
 
-        return result.scalars().all()
+        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
+
+    async def get_filtered_by_time(
+            self,
+            date_from: date,
+            date_to: date,
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
