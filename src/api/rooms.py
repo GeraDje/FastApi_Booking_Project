0ff1ebfdc706700676397697657e27f5c4412d3f
 mac <mac@MacBook-Pro-mac.local>
2026-02-28 15:41:47 +0300

    allow add facilities when adding rooms

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,10 +1,9 @@
 from datetime import date
 
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Query
 
 from src.api.dependencies import DBDep
-from src.database import async_session_maker
-from src.repositories.rooms import RoomsRepository
+from src.schemas.facilities import RoomFacilityAdd
 from src.schemas.rooms import RoomAdd, RoomAddRequest, RoomPatchRequest, RoomPatch
 
 router = APIRouter(prefix="/hotels", tags=["Номера"])
@@ -14,39 +13,33 @@ router = APIRouter(prefix="/hotels", tags=["Номера"])
 async def get_rooms(
         hotel_id: int,
         db: DBDep,
-        date_to: date,
-        date_from: date,
+        date_from: date = Query(example="2024-08-01"),
+        date_to: date = Query(example="2024-08-10"),
 ):
-    return await db.rooms.get_filtered(hotel_id=hotel_id,date_from=date_from, date_to=date_to)
+    return await db.rooms.get_filtered_by_time(hotel_id=hotel_id, date_from=date_from, date_to=date_to)
 
 
 @router.get("/{hotel_id}/rooms/{room_id}")
-async def get_room(
-        hotel_id: int,
-        room_id: int,
-        db: DBDep,
-):
+async def get_room(hotel_id: int, room_id: int, db: DBDep):
     return await db.rooms.get_one_or_none(id=room_id, hotel_id=hotel_id)
 
 
 @router.post("/{hotel_id}/rooms")
-async def create_room(
-        db: DBDep,
-        hotel_id: int,
-        room_data: RoomAddRequest = Body(),
-):
+async def create_room(hotel_id: int, db: DBDep, room_data: RoomAddRequest = Body()):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     room = await db.rooms.add(_room_data)
+    rooms_facilities_data = [RoomFacilityAdd(room_id=room.id, facility_id=f_id) for f_id in room_data.facilities_ids]
+    await db.rooms_facilities.add_bulk(rooms_facilities_data)
     await db.commit()
     return {"status": "OK", "data": room}
 
 
 @router.put("/{hotel_id}/rooms/{room_id}")
-async def update_room(
-        db: DBDep,
+async def edit_room(
         hotel_id: int,
         room_id: int,
-        room_data: RoomPatchRequest = Body(),
+        room_data: RoomAddRequest,
+        db: DBDep,
 ):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
     await db.rooms.edit(_room_data, id=room_id)
@@ -56,10 +49,10 @@ async def update_room(
 
 @router.patch("/{hotel_id}/rooms/{room_id}")
 async def partially_edit_room(
-        db: DBDep,
         hotel_id: int,
         room_id: int,
         room_data: RoomPatchRequest,
+        db: DBDep,
 ):
     _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
     await db.rooms.edit(_room_data, exclude_unset=True, id=room_id, hotel_id=hotel_id)
@@ -68,10 +61,7 @@ async def partially_edit_room(
 
 
 @router.delete("/{hotel_id}/rooms/{room_id}")
-async def delete_room(
-        db: DBDep,
-        hotel_id: int,
-        room_id: int):
+async def delete_room(hotel_id: int, room_id: int, db: DBDep):
     await db.rooms.delete(id=room_id, hotel_id=hotel_id)
     await db.commit()
-    return {"status": "ok"}
+    return {"status": "OK"}
--- src/repositories/base.py
+++ src/repositories/base.py
@@ -9,7 +9,7 @@ class BaseRepository:
     def __init__(self, session):
         self.session = session
 
-    async def get_filtered(self, *filter,**filter_by):
+    async def get_filtered(self, *filter, **filter_by):
         query = (
             select(self.model)
             .filter(*filter)
@@ -34,6 +34,12 @@ class BaseRepository:
         result = await self.session.execute(add_data_stmt)
         model = result.scalars().one()
         return self.schema.model_validate(model)
+
+    async def add_bulk(self, data: list[BaseModel]):
+        add_data_stmt = insert(self.model).values([items.model_dump() for items in data])
+        await self.session.execute(add_data_stmt)
+        await self.session.execute(add_data_stmt)
+
 
     async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by) -> None:
         update_stmt = (
