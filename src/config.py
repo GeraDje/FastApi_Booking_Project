49920df9a1d7c1cd9a753993fa2fc0f61aa69cc2
 mac <mac@MacBookPro.Dlink>
2026-03-10 11:42:20 +0300

    add celery

--- src/config.py
+++ src/config.py
@@ -24,6 +24,10 @@ class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
 
+    @property
+    def REDIS_URL(self):
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
+
     model_config = SettingsConfigDict(
         # pathlib позволяет формировать путь
         # с помощью оператора "/", аналогично os.path.join()
