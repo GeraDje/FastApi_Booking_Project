da8dbe4193bca518d9f0337145c171792f7e7a2e
 mac <mac@MacBook-Pro-mac.local>
2026-03-11 09:34:10 +0300

    add pytest-dotenv, .env-test

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
@@ -6,6 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class Settings(BaseSettings):
+    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"]
+
     DB_NAME: str
     DB_HOST: str
     DB_PORT: int
