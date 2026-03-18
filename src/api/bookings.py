60d39b53277fd80c9e805f07048c85b6be2c8c0d
 mac <mac@MacBookPro.Dlink>
2026-03-18 09:44:24 +0300

    add exceptions for POST /booking (part 2)

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi import HTTPException
 
 from src.api.dependencies import DBDep, UserIdDep
-from src.exceptions import ObjectNotFoundException
+from src.exceptions import ObjectNotFoundException, AllRoomsAreBookedException
 from src.schemas.bookings import BookingAddRequest, BookingAdd
 from src.schemas.hotels import Hotel
 from src.schemas.rooms import Room
@@ -22,9 +22,9 @@ async def get_my_bookings(user_id: UserIdDep, db: DBDep):
 
 @router.post("")
 async def add_booking(
-    user_id: UserIdDep,
-    db: DBDep,
-    booking_data: BookingAddRequest,
+        user_id: UserIdDep,
+        db: DBDep,
+        booking_data: BookingAddRequest,
 ):
     try:
         room: Room = await db.rooms.get_one(id=booking_data.room_id)
@@ -37,6 +37,9 @@ async def add_booking(
         price=room_price,
         **booking_data.dict(),
     )
-    booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
+    try:
+        booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
+    except AllRoomsAreBookedException as ex:
+        raise HTTPException(status_code=409, detail=ex.detail)
     await db.commit()
     return {"status": "OK", "data": booking}
